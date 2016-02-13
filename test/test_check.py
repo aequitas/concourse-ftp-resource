a7481dbbe7d9a314ad3e1a02fb51103b89094c33
Johan Bloemberg <github@ijohan.nl>
2016-02-13 19:51:22 +0100

    Generalize command execution to make tests readable

--- test/test_check.py
+++ test/test_check.py
@@ -3,6 +3,20 @@ import subprocess
 import sys
 
 
+def cmd(cmd_name, source, args=[], version={}, params={}):
+    """Wrap command interaction for easier use with python objects."""
+
+    in_json = json.dumps({
+        "source": source,
+        "version": version,
+        "params": params,
+    })
+    output = subprocess.check_output('/opt/resource/check',
+        stderr=sys.stderr, input=bytes(in_json, 'utf-8'))
+
+    return json.loads(output.decode())
+
+
 def test_check_one_file(ftp_root, ftp_server):
     """Test if one uploaded file returns one version."""
 
@@ -13,13 +27,7 @@ def test_check_one_file(ftp_root, ftp_server):
         "regex": "(?P<file>filename-(?P<version>.*).tgz)"
     }
 
-    in_json = json.dumps({
-        "source": source
-    })
-    output = subprocess.check_output('/opt/resource/check',
-        stderr=sys.stderr, input=bytes(in_json, 'utf-8'))
-
-    result = json.loads(output.decode())
+    result = cmd('in', source)
 
     assert result == [{"version": "0.0.0"}]
 
@@ -34,13 +42,7 @@ def test_check_multiple_files(ftp_root, ftp_server):
         "regex": "(?P<file>filename-(?P<version>.*).tgz)"
     }
 
-    in_json = json.dumps({
-        "source": source
-    })
-    output = subprocess.check_output('/opt/resource/check',
-        stderr=sys.stderr, input=bytes(in_json, 'utf-8'))
-
-    result = json.loads(output.decode())
+    result = cmd('in', source)
 
     assert result == [{"version": "0.0.1"}], 'should only return most recent version'
 
@@ -57,16 +59,7 @@ def test_check_passing_version(ftp_root, ftp_server):
         "regex": "(?P<file>filename-(?P<version>.*).tgz)"
     }
 
-    in_json = json.dumps({
-        "source": source,
-        "version": {
-            "version": "0.0.1"
-        }
-    })
-    output = subprocess.check_output('/opt/resource/check',
-        stderr=sys.stderr, input=bytes(in_json, 'utf-8'))
-
-    result = json.loads(output.decode())
+    result = cmd('in', source, version={"version": "0.0.1"})
 
     assert {"version": "0.0.2"} in result, 'new version should be in result'
     assert {"version": "0.0.3"} in result, 'new version should be in result'
@@ -84,15 +77,6 @@ def test_check_no_new_version(ftp_root, ftp_server):
         "regex": "(?P<file>filename-(?P<version>.*).tgz)"
     }
 
-    in_json = json.dumps({
-        "source": source,
-        "version": {
-            "version": "0.0.1"
-        }
-    })
-    output = subprocess.check_output('/opt/resource/check',
-        stderr=sys.stderr, input=bytes(in_json, 'utf-8'))
-
-    result = json.loads(output.decode())
+    result = cmd('in', source, version={"version": "0.0.1"})
 
     assert result == []
